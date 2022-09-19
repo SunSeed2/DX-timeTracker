5fa99d1be9d4d74ada005b68beb1e4b38d83967a
Konstantin Cherkasskikh <lgs.sunseed@gmail.com>
2022-09-19 13:36:08 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./assets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
